4b3b50c7949b8d16a945f54a7819b6ffe7a6e536
John Else <john.else@gmail.com>
2016-06-17 01:27:33 +0100

    Move the test runner code into a library
    Signed-off-by: John Else <john.else@gmail.com>

---
+++ runner/webtest_runner.ml
@@ -0,0 +1,40 @@
+module Html = Dom_html
+
+let start test _ =
+  let open Js.Unsafe in
+  let webtest = Js.Unsafe.obj [||] in
+  webtest##finished <- Js._false;
+  webtest##passed <- Js._false;
+  webtest##run <- Js.wrap_callback
+    (fun () ->
+      let {Webtest.log; results} = Webtest.run test in
+      let total, errored, failed, succeeded =
+        List.fold_left
+          (fun (total, errors, failures, successes) result ->
+            let open Webtest in
+            match result with
+            | Error _ -> total + 1, errors + 1, failures, successes
+            | Failure _ -> total + 1, errors, failures + 1, successes
+            | Success -> total + 1, errors, failures, successes + 1)
+          (0, 0, 0, 0) results
+      in
+      let final_log =
+        String.concat "\n" [
+          log;
+          Printf.sprintf "%d tests run" total;
+          Printf.sprintf "%d errors" errored;
+          Printf.sprintf "%d failures" failed;
+          Printf.sprintf "%d succeeded" succeeded;
+        ]
+      in
+      let passed = total = succeeded in
+      webtest##log <- Js.string final_log;
+      webtest##passed <- if passed then Js._true else Js._false;
+      webtest##finished <- Js._true);
+
+  global##webtest <- webtest;
+
+  Js._false
+
+let setup test =
+  Html.window##onload <- Html.handler (start test)
---
+++ runner/webtest_runner.mli
@@ -0,0 +1,1 @@
+val setup : Webtest.test -> unit
