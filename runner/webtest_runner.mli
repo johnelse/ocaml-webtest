994bedd2b21f5c60c53aab301b76511ad128f720
John Else <john.else@gmail.com>
2016-06-17 01:31:33 +0100

    Add doccomments
    Signed-off-by: John Else <john.else@gmail.com>

--- runner/webtest_runner.mli
+++ runner/webtest_runner.mli
@@ -1,1 +1,5 @@
+(** Types and functions for running tests in a browser. *)
+
 val setup : Webtest.test -> unit
+(** [setup test] sets up a test runner and attaches it to the document's onLoad
+    handler. *)
