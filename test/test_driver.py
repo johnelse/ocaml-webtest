6051365ac5d7f5c995811e06528a3f6fe6ef0670
John Else <john.else@gmail.com>
2016-07-12 22:11:44 +0100

    Make test_driver.py pep8 compliant
    Signed-off-by: John Else <john.else@gmail.com>

--- test/test_driver.py
+++ test/test_driver.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+"""
+Selenium test runner.
+"""
 
 import os
 import sys
 
-driver = webdriver.Firefox()
-driver.get("file://%s" % (os.path.join(os.getcwd(), "test/test_runner.html")))
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
-WebDriverWait(driver, 10).until(
-    lambda driver: driver.execute_script("return (window.webtest != undefined)"))
 
-driver.execute_script("webtest.run()")
+def main():
+    """
+    Main program.
+    """
+    driver = webdriver.Firefox()
+    driver.get(
+        "file://%s" % (os.path.join(os.getcwd(), "test/test_runner.html")))
 
-WebDriverWait(driver, 10).until(
-    lambda driver: driver.execute_script("return webtest.finished"))
+    WebDriverWait(driver, 10).until(
+        lambda driver:
+            driver.execute_script("return (window.webtest != undefined)"))
 
-webtest = driver.execute_script("return webtest")
-print webtest["log"]
+    driver.execute_script("webtest.run()")
 
-driver.close()
+    WebDriverWait(driver, 10).until(
+        lambda driver: driver.execute_script("return webtest.finished"))
 
-if not webtest["passed"]:
-    sys.exit(1)
+    webtest = driver.execute_script("return webtest")
+    print webtest["log"]
+
+    driver.close()
+
+    if not webtest["passed"]:
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
