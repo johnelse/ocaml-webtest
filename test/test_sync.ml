50309007bc4c98bac53ddc734b04d2af998e495c
John Else <john.else@gmail.com>
2016-09-03 22:31:15 +0100

    Add more tests of Sync.bracket
    Signed-off-by: John Else <john.else@gmail.com>

--- test/test_sync.ml
+++ test/test_sync.ml
@@ -25,7 +25,7 @@ let test_assert_equal_printer () =
     failwith "assert_equal should have failed"
   with TestFailure "not equal: 5 6" -> ()
 
-let test_bracket () =
+let test_bracket_succeed () =
   let state = ref `uninitialised in
   let setup () = state := `test_start; state in
   let teardown state =
@@ -40,6 +40,38 @@ let test_bracket () =
     teardown ();
   assert_equal !state `torn_down
 
+let test_bracket_fail () =
+  let state = ref `uninitialised in
+  let setup () = state := `test_start; state in
+  let teardown state =
+    state := `torn_down
+  in
+  try
+    Sync.bracket
+      setup
+      (fun state ->
+        assert_equal !state `test_start;
+        assert_equal 5 6)
+      teardown ();
+  with TestFailure "not equal" ->
+    assert_equal !state `torn_down
+
+let test_bracket_error () =
+  let state = ref `uninitialised in
+  let setup () = state := `test_start; state in
+  let teardown state =
+    state := `torn_down
+  in
+  try
+    Sync.bracket
+      setup
+      (fun state ->
+        assert_equal !state `test_start;
+        failwith "error")
+      teardown ();
+  with Failure "error" ->
+    assert_equal !state `torn_down
+
 let suite =
   "sync" >::: [
     "test_assert_true_ok" >:: test_assert_true_ok;
@@ -47,5 +79,7 @@ let suite =
     "test_assert_equal_ok" >:: test_assert_equal_ok;
     "test_assert_equal_fail" >:: test_assert_equal_fail;
     "test_assert_equal_printer" >:: test_assert_equal_printer;
-    "test_bracket" >:: test_bracket;
+    "test_bracket_succeed" >:: test_bracket_succeed;
+    "test_bracket_fail" >:: test_bracket_fail;
+    "test_bracket_error" >:: test_bracket_error;
   ]
