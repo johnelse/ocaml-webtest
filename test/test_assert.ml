e2a32248d7de4c8106ecdfed55a61a7f244cac83
John Else <john.else@gmail.com>
2016-10-18 01:33:25 +0100

    Add some tests of the label printing code
    Signed-off-by: John Else <john.else@gmail.com>

--- test/test_assert.ml
+++ test/test_assert.ml
@@ -22,11 +22,23 @@ let test_assert_equal_fail () =
     failwith "assert_equal should have failed"
   with TestFailure "not equal" -> ()
 
+let test_assert_equal_label () =
+  try
+    assert_equal ~label:"assert_equal" 5 6;
+    failwith "assert_equal should have failed"
+  with TestFailure "not equal (assert_equal)" -> ()
+
 let test_assert_equal_printer () =
   try
     assert_equal ~printer:string_of_int 5 6;
     failwith "assert_equal should have failed"
   with TestFailure "not equal: 5 6" -> ()
+
+let test_assert_equal_label_printer () =
+  try
+    assert_equal ~label:"assert_equal" ~printer:string_of_int 5 6;
+    failwith "assert_equal should have failed"
+  with TestFailure "not equal (assert_equal): 5 6" -> ()
 
 let test_assert_raises_ok () =
   assert_raises (MyException 0) (fun () -> raise (MyException 0))
@@ -39,6 +51,23 @@ let test_assert_raises_wrong_exn () =
   try assert_raises (MyException 0) (fun () -> raise (MyException 1))
   with
    | TestFailure "unexpected exception raised: Test_assert.MyException(1)" -> ()
+
+let test_assert_raises_no_exn_label () =
+  try
+    assert_raises ~label:"assert_raises" (MyException 0) (fun () -> ())
+  with
+   | TestFailure "expected exception not raised (assert_raises)" -> ()
+
+let test_assert_raises_wrong_exn_label () =
+  try
+    assert_raises
+      ~label:"assert_raises"
+      (MyException 0)
+      (fun () -> raise (MyException 1))
+  with
+   | TestFailure
+       "unexpected exception raised (assert_raises): Test_assert.MyException(1)"
+     -> ()
 
 let test_assert_raises_string_ok () =
   assert_raises_string
@@ -63,10 +92,14 @@ let suite =
     "test_assert_true_fail" >:: test_assert_true_fail;
     "test_assert_equal_ok" >:: test_assert_equal_ok;
     "test_assert_equal_fail" >:: test_assert_equal_fail;
+    "test_assert_equal_label" >:: test_assert_equal_label;
     "test_assert_equal_printer" >:: test_assert_equal_printer;
+    "test_assert_equal_label_printer" >:: test_assert_equal_label_printer;
     "test_assert_raises_ok" >:: test_assert_raises_ok;
     "test_assert_raises_no_exn" >:: test_assert_raises_no_exn;
     "test_assert_raises_wrong_exn" >:: test_assert_raises_wrong_exn;
+    "test_assert_raises_no_exn_label" >:: test_assert_raises_no_exn_label;
+    "test_assert_raises_wrong_exn_label" >:: test_assert_raises_wrong_exn_label;
     "test_assert_raises_string_ok" >:: test_assert_raises_string_ok;
     "test_assert_raises_string_no_exn" >:: test_assert_raises_string_no_exn;
     "test_assert_raises_string_wrong_exn" >::
